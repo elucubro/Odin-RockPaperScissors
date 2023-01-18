54b93d88715f106fdd990ec39ee90a112d7a55b1
xescr <114061880+xescr@users.noreply.github.com>
2023-01-18 19:59:14 +0800

    this is me finally finding a sorting algorithm that works the way it does is via first finding if it is a tie, if not it sorts into rock player choice scissors player choice ect, then does the same for computerchoice!

---
+++ js-css/scripts.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"]
+    const random = Math.floor(Math.random() * options.length);
+    return(options[random]);
+}
+
+function singlernd(playerSelection) {
+    if (playerSelection.toLowerCase() == getComputerChoice()) {
+        return 'tie!';
+    } else if (playerSelection.toLowerCase() == 'rock') {
+        return 'Worked!';
+    }
+}
+
+console.log(singlernd(ROcK));
+
+/* psuedocode
+is playerchoice equal to computerchoice? > yes = tie no = continue*/
