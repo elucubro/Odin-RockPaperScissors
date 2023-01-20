e86c97ecc52c894ea2752efad547627c54b40e54
xescr <114061880+xescr@users.noreply.github.com>
2023-01-20 12:38:10 +0800

    added leaderboard functionality

--- js-css/scripts.js
+++ js-css/scripts.js
@@ -1,8 +1,3 @@
-// Creating Rock Paper Container, Including Container Class
-const rockPaperContainer = document.createElement('div')
-rockPaperContainer.classList.add("rockPaperContainer")
-document.body.appendChild(rockPaperContainer)
-
 // Random Option Function Declaration
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"]
@@ -15,35 +10,71 @@ function singlernd(playerSelection) {
     let computerChoice = getComputerChoice();
     let playerSelect = playerSelection.toLowerCase();
     console.log(playerSelect)
+    // Dealing With Tie Condition
     if (playerSelect == computerChoice) {
-        return 'tie! ';
+        return 'tie ';
+    // Sorting all possible conditions
     } else if (playerSelect == "rock"){
         if (computerChoice = "scissors") {
-            return 'win! ';
+            return 'win ';
         }
         if (computerChoice = "paper") {
-            return 'loss! ';
+            return 'loss ';
         }
 
     } else if (playerSelect == "paper"){
         if (computerChoice = "rock") {
-            return 'win! ';
+            return 'win ';
         }
         if (computerChoice = "scissors") {
-            return 'loss! ';
+            return 'loss ';
         }
 
     } else if (playerSelect == "scissors"){
         if (computerChoice = "paper") {
-            return 'win! ';
+            return 'win ';
         }
         if (computerChoice = "rock") {
-            return 'loss! ';
+            return 'loss ';
         }
 
     }
 }
 
+// Creating Rock Paper Container, Including Container Class
+const rockPaperContainer = document.createElement('div')
+rockPaperContainer.classList.add("rockPaperContainer")
+// Creating Leaderboard Container
+const leaderboardContainer = document.createElement('div')
+leaderboardContainer.classList.add("leaderboardContainer")
+// Creating Buttons 
+const rockButton = document.createElement('button');
+const paperButton = document.createElement('button');
+const scissorsButton = document.createElement('button');
+
+rockButton.textContent = 'R'
+paperButton.textContent = 'P'
+scissorsButton.textContent = 'S'
+// Create Functions To Pass Button Conditions
+function buttonSelection(Item, RPS) {
+    let initrndResults = singlernd(Item);
+    rndResults = initrndResults.substring(0,2)
+    let divLeaderboard = document.createElement('div')
+    divLeaderboard.classList.add("leaderboardOption")
+    divLeaderboard.textContent = initrndResults
+    leaderboardContainer.appendChild(divLeaderboard)
+}
+rockButton.addEventListener('click', function() {buttonSelection("Rock", rockButton)})
+paperButton.addEventListener('click', function() {buttonSelection("Paper", paperButton)})
+scissorsButton.addEventListener('click', function() {buttonSelection("Scissors", scissorsButton)})
+
+// Append Buttons To Container
+rockPaperContainer.appendChild(rockButton)
+rockPaperContainer.appendChild(paperButton)
+rockPaperContainer.appendChild(scissorsButton)
+//Appending Container to Body
+document.body.appendChild(rockPaperContainer)
+rockPaperContainer.appendChild(leaderboardContainer)
 
 /* psuedocode
 is playerchoice equal to computerchoice? > yes = tie no = continue*/
