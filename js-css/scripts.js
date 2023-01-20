5e7ed30ad9eba69bac47474207a980d3b81abe6c
xescr <114061880+xescr@users.noreply.github.com>
2023-01-20 11:42:42 +0800

    Had to redo due to losing files on to local machine, back to console only mode not and commits added

--- js-css/scripts.js
+++ js-css/scripts.js
@@ -1,18 +1,49 @@
+// Creating Rock Paper Container, Including Container Class
+const rockPaperContainer = document.createElement('div')
+rockPaperContainer.classList.add("rockPaperContainer")
+document.body.appendChild(rockPaperContainer)
+
+// Random Option Function Declaration
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"]
     const random = Math.floor(Math.random() * options.length);
     return(options[random]);
 }
 
+// Single Round Function, Plays through one round.
 function singlernd(playerSelection) {
-    if (playerSelection.toLowerCase() == getComputerChoice()) {
-        return 'tie!';
-    } else if (playerSelection.toLowerCase() == 'rock') {
-        return 'Worked!';
+    let computerChoice = getComputerChoice();
+    let playerSelect = playerSelection.toLowerCase();
+    console.log(playerSelect)
+    if (playerSelect == computerChoice) {
+        return 'tie! ';
+    } else if (playerSelect == "rock"){
+        if (computerChoice = "scissors") {
+            return 'win! ';
+        }
+        if (computerChoice = "paper") {
+            return 'loss! ';
+        }
+
+    } else if (playerSelect == "paper"){
+        if (computerChoice = "rock") {
+            return 'win! ';
+        }
+        if (computerChoice = "scissors") {
+            return 'loss! ';
+        }
+
+    } else if (playerSelect == "scissors"){
+        if (computerChoice = "paper") {
+            return 'win! ';
+        }
+        if (computerChoice = "rock") {
+            return 'loss! ';
+        }
+
     }
 }
 
-console.log(singlernd(ROcK));
 
 /* psuedocode
 is playerchoice equal to computerchoice? > yes = tie no = continue*/
